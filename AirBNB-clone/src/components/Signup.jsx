5ee2a8d20f7283a3d784ddb4a6ddba56f9fe099f
Prashanth Thiagarajan <prshnth.619@gmail.com>
2023-01-18 18:29:20 +0530

    Enhanced sign up form

--- AirBNB-clone/src/components/Signup.jsx
+++ AirBNB-clone/src/components/Signup.jsx
@@ -4,31 +4,43 @@ import { useState } from "react"
 export default function Signup(props){
 
     return(props.trigger) ? (
-        <div className="signup">
+        <div className="signup" div="unblurred">
+            <div className="signup-header" div="unblurred">
                 <button className="signup-close-btn" onClick={()=>props.setTrigger(false)}>X</button>
-                {props.children}
-                <div class="signup-form">
+                <h4>Log in or sign up</h4>
+                </div>
+                {/* {props.children} */}
+                <div className="signup-form">
                 <h2>Welcome to AirBNB</h2>
-                <input type="text" placeholder="Country/region" />
-                <input type="text" placeholder="Phone Number" />
-                <p>We’ll call or text you to confirm your number. Standard message and data rates apply. <u>Privacy Policy</u></p>
+                <input type="text" placeholder="Country/region" className="signup-textbox signup-textbox-country"/>
+                <input type="text" placeholder="Phone Number" className="signup-textbox signup-textbox-phone"/>
+                <p className="signup-privacypolicy">We’ll call or text you to confirm your number. Standard message and data rates apply. <u>Privacy Policy</u></p>
                 <button class="signup-continue-button">Continue</button>
                 <p>or</p>
-                <button class="signup-socialmedia-login-button">
-                    <img src="" />
-                    <p>Continue with Facebook</p>
+                <button class="social-media-button signup-socialmedia-login-button">
+                <div class="button-content-flex">
+                    <div><a href="#" class="fa fa-facebook"></a></div>
+                    <div></div>
+                    <div><p>Continue with Facebook</p></div>
+                    </div>
                 </button>
-                <button class="signup-socialmedia-login-button">
-                    <img src="" />
-                    <p>Continue with Google</p>
+                <button class="social-media-button signup-socialmedia-login-button">
+                    <div class="button-content-flex">
+                    <div><a href="#" class="fa fa-google"></a></div>
+                    <div><p>Continue with Google</p></div>
+                    </div>
                 </button>
-                <button class="signup-socialmedia-login-button">
-                    <img src="" />
-                    <p>Continue with Apple</p>
+                <button class="social-media-button signup-socialmedia-login-button">
+                <div class="button-content-flex">
+                    <div><a href="#" class="fa fa-apple"></a></div>
+                    <div><p>Continue with Apple</p></div>
+                    </div>
                 </button>
-                <button class="signup-socialmedia-login-button">
-                    <img src="" />
-                    <p>Continue with email</p>
+                <button class="social-media-button signup-socialmedia-login-button">
+                <div class="button-content-flex">
+                    <div><a href="#"><i class="fa fa-envelope"></i></a></div>
+                    <div><p>Continue with email</p></div>
+                    </div>
                 </button>
                 </div>
         </div>
